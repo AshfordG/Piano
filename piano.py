71a946e86f0b901c3a59dd39b3e8e87be95c719e
AshfordG <ashford.gavin@menloschool.org>
2024-04-23 09:52:52 -0700

    piano constructor

---
+++ piano.py
@@ -0,0 +1,105 @@
+import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import cv2
+
+# Library Constants
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkPoints = mp.solutions.hands.HandLandmark
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+DrawingUtil = mp.solutions.drawing_utils
+
+WHITE = (255, 255, 255)
+
+
+class PianoKey:
+    def __init__(self, color, screen_width=600, screen_height=400):
+        self.color = color
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+    
+    def draw_key(self, image, pt1, pt2):
+        cv2.rectangle(image, (pt1), (pt2), self.color, -1)
+
+
+class Game:
+    def __init__(self):
+        self.keys = []
+
+        #Initialize keys
+        self.KeyA = PianoKey(WHITE)
+
+        # Create the hand detector
+        base_options = BaseOptions(model_asset_path='data/hand_landmarker.task')
+        options = HandLandmarkerOptions(base_options=base_options,
+                                                num_hands=2)
+        self.detector = HandLandmarker.create_from_options(options)
+
+        # TODO: Load video
+        self.video = cv2.VideoCapture(1)
+
+    
+    def draw_landmarks_on_hand(self, image, detection_result):
+        # Get a list of the landmarks
+        hand_landmarks_list = detection_result.hand_landmarks
+        
+        # Loop through the detected hands to visualize.
+        for idx in range(len(hand_landmarks_list)):
+            hand_landmarks = hand_landmarks_list[idx]
+
+            # Save the landmarks into a NormalizedLandmarkList
+            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            hand_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+            ])
+
+            # Draw the landmarks on the hand
+            DrawingUtil.draw_landmarks(image,
+                                       hand_landmarks_proto,
+                                       solutions.hands.HAND_CONNECTIONS,
+                                       solutions.drawing_styles.get_default_hand_landmarks_style(),
+                                       solutions.drawing_styles.get_default_hand_connections_style())
+
+    def run(self):
+        while self.video.isOpened():
+            # Get the current frame
+            frame = self.video.read()[1]
+
+            # Convert it to an RGB image
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # The image comes mirrored - flip it
+            image = cv2.flip(image, 1)
+
+            #Draw Piano
+            #loop through number of keys we want, self.
+            cv2.rectangle(image, (600, 300), (675, 500), WHITE, -1)
+            # Height = 300
+            #WIdth  = 80
+
+            # Convert the image to a readable format and find the hands
+            to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+            results = self.detector.detect(to_detect)
+
+            # Draw the hand landmarks
+            self.draw_landmarks_on_hand(image, results)
+
+            # Change the color of the frame back
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            cv2.imshow('Hand Tracking', image)
+
+            # Break the loop if the user presses 'q'
+            if cv2.waitKey(50) & 0xFF == ord('q'):
+                print(self.score)
+                break
+        
+        # Release our video and close all windows
+        self.video.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":        
+    g = Game()
+    g.run()
